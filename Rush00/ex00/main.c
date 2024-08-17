82063e37994dcde6c40121d9f1fb2150c3e71fc0
Meral Hamarat <meralhamarat24@gmail.com>
2024-08-17 13:13:12 +0300

    Add files via upload

---
+++ Rush00/ex00/main.c
@@ -0,0 +1,8 @@
+void ft_putchar(char c);
+void rush(int x, int y);
+
+int main()
+{
+    rush(5,5);
+    return(0);
+}
---
+++ Rush00/ex00/rush00.c
@@ -0,0 +1,29 @@
+void    ft_putchar(char c);
+
+void    rush(int x,int y)
+{
+    int x_c;
+    int y_c;
+    if(x < 1 || y < 1)
+        return;
+
+    y_c = 1;
+    while (y_c <= y)
+    {
+        x_c = 1;
+        while (x_c <= x)
+        {
+            if ((x_c == 1 && y_c == 1) || (x_c == x && y_c == y ) || ((x_c == 1 && y_c == y) || (x_c == x && y_c == 1)) )
+                ft_putchar('o');
+            else if ((y_c == 1 && x_c > 1 && x_c < x) || (y_c == y && x_c > 1 && x_c < x))
+                ft_putchar('-');
+            else if ((x_c == 1 && y_c > 1 && y_c < y) || (x_c == x && y_c > 1 && y_c < y))
+                ft_putchar('|');
+            else
+                ft_putchar(' ');
+            x_c++;
+        }
+        ft_putchar('\n');
+        y_c++;
+    }
+}
