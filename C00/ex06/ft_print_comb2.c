70c656fd7b5e6e1df4f5edb1885f7c1343e7d9c5
Meral Hamarat <meralhamarat24@gmail.com>
2024-08-18 11:53:28 +0300

    Add files via upload

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mehamara <mehamara@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 19:03:58 by mehamara          #+#    #+#             */
+/*   Updated: 2024/08/17 20:18:22 by mehamara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	n1;
+	int	n2;
+
+	n1 = 0;
+	while (n1 <= 98)
+	{
+		n2 = n1 + 1;
+		while (n2 <= 99)
+		{
+			ft_putchar((n1 / 10) + 48);
+			ft_putchar((n1 % 10) + 48);
+			ft_putchar(' ');
+			ft_putchar((n2 / 10) + 48);
+			ft_putchar((n2 % 10) + 48);
+			if (n1 != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			n2++;
+		}
+		n1++;
+	}
+}
